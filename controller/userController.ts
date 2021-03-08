09cc00f36a75acf398b07267ea1b9157ac3d37fb
Brian Mendoza <brian.mendoza.mendoza@gmail.com>
2021-03-08 01:38:48 -0500

    endpoint user completado

--- controller/userController.ts
+++ controller/userController.ts
@@ -3,74 +3,154 @@ import User from "../models/user";
 
 
 export const getLtUsers = async ( req: Request, res: Response ) => {
-    const usuarios = await User.findAll();
+    
+    try {
 
-    res.json( { usuarios } );
+        const usuarios = await User.findAll( {
+            where: { active: true }
+        } )
+
+        res.json( { usuarios } );
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json( { msg: 'Algo falló, comuníquese con soporte' } );
+    }
+
+    
 }
 
 
 
 export const getUser =  async ( req: Request, res: Response ) => {
     const { id } = req.params;
-    const usuario = await User.findByPk( id );
+    
+    try {
 
-    if ( !usuario ) {
-        return res.status(404).json( { 
-            msg: `No existe usuario con el id ${ id }` } )
+        const usuario = await User.findByPk( id );
+        if ( !usuario ) {
+            return res.status(404).json( { 
+                msg: `No existe usuario con el id ${id}` } )
+        }
+
+        return res.json( { usuario } );
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json( { msg: 'Algo falló, comuníquese con soporte' } );
     }
     
-    res.json( { usuario } );
+    
 }
 
 
 
 export const createdUser = async ( req: Request, res: Response ) => {
     const { body } = req;
+    const { nombres, apellidos, correo, dni } = body;
 
     try {
 
+        if ( !nombres || !apellidos || !dni ) {
+            return res.status(400).json( { 
+                msg: `Los campos ${!nombres ? '[nombres]' : ''} ${!apellidos ? '[apellidos]' : ''} ${!dni ? '[dni]' : ''} son obligatorios` 
+            } );
+        }
+
         const userWithSameDNI = await User.findOne( {
-            where: { dni: body.dni }
+            where: { dni }
         } );
-        
 
         if ( userWithSameDNI ) {
             return res.status(400).json( {
-                msg: `Ya existe un usuario con DNI: ${ body.dni } con el nombre: ${ userWithSameDNI.name }`
+                msg: `Ya existe un usuario con DNI: ${dni} con el nombre: ${ userWithSameDNI.name }`
             } )
         }
 
-        //const usuario = User. // new User(body);
-        const usuario = await User.create(body);
+        const usuario = await User.create( {
+            name: nombres,
+            lastname: apellidos,
+            email: correo,
+            dni
+        } );
 
         res.json( { usuario } );
 
     } catch ( error ) {
         console.log(error);
-        res.status(500).json( { 
-            msg: 'Algo falló, comuníquese con soporte' } )
+        return res.status(500).json( { msg: 'Algo falló, comuníquese con soporte' } );
     }
 
 }
 
 
-export const updatedUser = ( req: Request, res: Response ) => {
+
+
+export const updatedUser =  async ( req: Request, res: Response ) => {
     const { id } = req.params;
     const { body } = req;
+    const { nombres, apellidos, correo } = body;
 
-    res.json( { 
-        msg: 'updatedUser',
-        id,
-        body } )
+    try {
+
+        let userUpdt = await User.findByPk( id );
+        if ( !userUpdt ) {
+            return res.status(404).json( { 
+                msg: `No existe usuario con el id ${id}` } )
+        }
+
+        await User.update( { 
+                name: nombres ? nombres : userUpdt.name,
+                lastname: apellidos ? apellidos : userUpdt.lastname,
+                email: correo ? correo : userUpdt.email
+            }, { 
+                where: {
+                    idUser: id } 
+            } 
+        );
+
+        userUpdt = await User.findByPk( id );
+
+        return res.json( { usuario: userUpdt } );
+ 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json( { msg: 'Algo falló, comuníquese con soporte' } );
+    }
+
 }
 
 
 
 
-export const deletedUser = ( req: Request, res: Response ) => {
+export const deletedUser =  async ( req: Request, res: Response ) => {
     const { id } = req.params;
 
-    res.json( { 
-        msg: 'deletedUser',
-        id } )
+    try {
+
+        let userDlt = await User.findByPk( id );
+        if ( !userDlt ) {
+            return res.status(404).json( { 
+                msg: `No existe usuario con el id ${ id }` } )
+        }
+
+        if ( userDlt && !userDlt.active ) {
+            return res.status(400).json( { 
+                msg: `El usuario ${userDlt.name} ${userDlt.lastname} ya ha sido eliminado anteriormente.` } );
+        }
+
+        await User.update( { 
+                active: false
+            }, { 
+                where: {
+                    idUser: id } 
+            } 
+        );
+
+        return res.json( { msg: `Se eliminó al usuario ${userDlt.name} ${userDlt.lastname} correctamente.` } );
+ 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json( { msg: 'Algo falló, comuníquese con soporte' } );
+    }
 }
--- db/connection.ts
+++ db/connection.ts
@@ -4,7 +4,7 @@ const db = new Sequelize('demo_node', 'postgres', 'postgres', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
-    //logging: false
+    logging: false
 });
 
 export default db;
